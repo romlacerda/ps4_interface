c2e2357b081c83b8fecc48c2f1b2580f0c02458f
Romulo Lacerda <rlcrweb@gmail.com>
2020-06-10 18:06:16 -0300

    feat: images

--- src/components/Box/styles.js
+++ src/components/Box/styles.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 const Box = styled.div`
-  height: ${(props) => (props.selected ? '370px' : '270px')};
-  width: ${(props) => (props.selected ? '370px' : '270px')};
+  height: ${(props) => (props.selected ? '280px' : '200px')};
+  width: ${(props) => (props.selected ? '280px' : '200px')};
   background-color: blue;
   margin: 0.2%;
   transition: width 0.2s, height 0.2s;
-  
+  border: ${(props) => (props.selected ? '1px solid #6eb1df' : 'none')};
+  box-shadow: ${(props) => (props.selected ? '0px 0px 10px #4775bf, 0px 0px 10px #4775bf inset' : 'none')};
+  background-image: url(${(props) => props.image});
+  background-position: 50% 30%;
+  background-size: cover;
 `;
 
 export default Box;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+  useState, useEffect, useRef, useCallback,
+} from 'react';
 import Box from '../../components/Box/styles';
 import Wrapper from '../../components/Wrapper/styles';
 // import { usePrevious } from '../../hooks';
+import ResidentEvil from '../../assets/games/re-3.svg';
+import Fifa from '../../assets/games/fifa-20.svg';
+import PsPlus from '../../assets/games/plus-2.svg';
+import StarWars from '../../assets/games/star-wars.svg';
 
 const Home = () => {
   const [selectedBox, setSelectedBox] = useState(0);
   const prevSelectedBoxRef = useRef();
-  const baseX = 10;
-  const baseWidth = 270;
+  const baseX = 15;
+  const baseWidth = 200;
   // const prevSelectedBox = usePrevious(selectedBox);
+
 
   useEffect(() => {
     prevSelectedBoxRef.current = selectedBox;
@@ -20,8 +27,27 @@ const Home = () => {
     setSelectedBox(id);
   }
 
+  function handleChangeKey(e) {
+    if (e.keyCode !== 37 && e.keyCode !== 39) {
+      return;
+    }
+
+    if (e.keyCode === 39) {
+      setSelectedBox(selectedBox + 1);
+    } else if (e.keyCode === 37) {
+      setSelectedBox(selectedBox - 1);
+    }
+  }
+
+  const cb = useCallback((e) => {
+    handleChangeKey(e);
+  }, []);
+
+  document.addEventListener('keydown', (e) => handleChangeKey(e));
+
   useEffect(() => {
     // const way = selectedBox > prevSelectedBox ? '-' : '+';
+    console.log('renderizou');
     const newX = selectedBox === 0 ? baseWidth : selectedBox * baseWidth;
     if (selectedBox === 0) {
       document.getElementById('boxes').style.transform = `translate(${baseX}%, 0)`;
@@ -31,24 +57,18 @@ const Home = () => {
   }, [selectedBox]);
 
 
-  function handleChangeKey(e) {
-    const direction = e.key.code === 39 ? 'right' : 'left';
-    setSelectedBox(direction === 'right' ? e.target.id + 1 : e.target.id - 1);
-  }
-
   return (
     <>
       <Wrapper>
-        <div id="boxes" style={{ display: 'flex' }}>
-          <Box id={0} onClick={handleChangeBox} onKeyDown={handleChangeKey} selected={selectedBox === 0} />
-          <Box id={1} onClick={handleChangeBox} selected={selectedBox === 1} />
-          <Box id={2} onClick={handleChangeBox} selected={selectedBox === 2} />
-          <Box id={3} onClick={handleChangeBox} selected={selectedBox === 3} />
+        <div id="boxes" style={{ display: 'flex', marginTop: '40px' }}>
+          <Box id={0} onClick={handleChangeBox} selected={selectedBox === 0} image={PsPlus} />
+          <Box id={1} onClick={handleChangeBox} selected={selectedBox === 1} image={StarWars} />
+          <Box id={2} onClick={handleChangeBox} selected={selectedBox === 2} image={ResidentEvil} />
+          <Box id={3} onClick={handleChangeBox} selected={selectedBox === 3} image={Fifa} />
           <Box id={4} onClick={handleChangeBox} selected={selectedBox === 4} />
           <Box id={5} onClick={handleChangeBox} selected={selectedBox === 5} />
         </div>
       </Wrapper>
-
     </>
   );
 };
