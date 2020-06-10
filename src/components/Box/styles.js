2aa7d10c148566d209b959ec25a2e0d999b28ba1
Romulo Lacerda <rlcrweb@gmail.com>
2020-06-10 13:55:46 -0300

    feat: width and height transition on select

--- src/components/Box/styles.js
+++ src/components/Box/styles.js
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
 const Box = styled.div`
-  width: 270px;
-  height: 270px;
+  height: ${(props) => (props.selected ? '370px' : '270px')};
+  width: ${(props) => (props.selected ? '370px' : '270px')};
   background-color: blue;
-  /* margin: 0.2%; */
-  border: ${(props) => {
-    console.log(props);
-    return (props.selected ? '1px solid red' : '');
-  }}
+  margin: 0.2%;
+  transition: width 0.2s, height 0.2s;
   
 `;
 
